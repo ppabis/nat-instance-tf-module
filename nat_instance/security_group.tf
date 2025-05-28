bd22cc97c5aa29cfbb21c2889f287990a35f5998
ppabis <piotr@pabis.eu>
2025-05-28 21:07:11 +0200

    Create Security Group of the NAT Instance.
    It allows complete egress traffic and ingress traffic from the resources
    associated with the same security group.

---
+++ nat_instance/security_group.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "security_group" {
+  vpc_id      = var.vpc_id
+  description = "Security group for NAT instance"
+  name        = "NATInstanceSecurityGroup"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+}
