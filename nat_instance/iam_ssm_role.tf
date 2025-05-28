d851f5faa02ab308622cadf177566a2666aa570b
ppabis <piotr@pabis.eu>
2025-05-28 21:15:41 +0200

    Update IAM SSM role to be created conditionally.
    Using count parameter to create the role only if requested by the user.

--- nat_instance/iam_ssm_role.tf
+++ nat_instance/iam_ssm_role.tf
@@ -11,14 +11,17 @@ data "aws_iam_policy_document" "assume_role_policy" {
 resource "aws_iam_role" "ssm_role" {
   name               = "NATInstanceSSMRole"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  count              = var.create_ssm_role ? 1 : 0
 }
 
 resource "aws_iam_instance_profile" "ssm_instance_profile" {
-  name = "NATInstanceSSMInstanceProfile"
-  role = aws_iam_role.ssm_role.name
+  name  = "NATInstanceSSMInstanceProfile"
+  role  = aws_iam_role.ssm_role[0].name
+  count = var.create_ssm_role ? 1 : 0
 }
 
 resource "aws_iam_role_policy_attachment" "ssm_role_policy_attachment" {
-  role       = aws_iam_role.ssm_role.name
+  role       = aws_iam_role.ssm_role[0].name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  count      = var.create_ssm_role ? 1 : 0
 }
