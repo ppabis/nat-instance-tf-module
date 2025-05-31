bfccb47ec779a4a79800fcb58cb68d7bfa07c06c
ppabis <piotr@pabis.eu>
2025-05-31 20:50:46 +0200

    Add a random S3 bucket for testing gateway connectivity

---
+++ test_infra/s3.tf
@@ -0,0 +1,32 @@
+resource "aws_s3_bucket" "optional_test_bucket" {
+  count  = var.test_bucket_name == "" ? 0 : 1
+  bucket = var.test_bucket_name
+  tags   = { Name = "test-bucket-${var.test_bucket_name}" }
+}
+
+resource "aws_s3_bucket_policy" "optional_test_bucket" {
+  count  = var.test_bucket_name == "" ? 0 : 1
+  bucket = aws_s3_bucket.optional_test_bucket[0].id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = aws_iam_role.ec2.arn
+        }
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket",
+          "s3:GetBucketLocation"
+        ]
+        Resource = [
+          aws_s3_bucket.optional_test_bucket[0].arn,
+          "${aws_s3_bucket.optional_test_bucket[0].arn}/*"
+        ]
+      }
+    ]
+  })
+}
