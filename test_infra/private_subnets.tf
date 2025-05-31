251fc4aadde574efc8231da7bb74e56d987a61e1
ppabis <piotr@pabis.eu>
2025-05-31 07:29:39 +0200

    Create a lot of private subnets for testing

---
+++ test_infra/private_subnets.tf
@@ -0,0 +1,25 @@
+data "aws_availability_zones" "azs" {}
+
+resource "aws_route_table" "private_route_table" {
+  count  = var.test_count
+  vpc_id = aws_vpc.vpc.id
+  tags   = { Name = "test-private-route-table" }
+  route {
+    cidr_block           = "0.0.0.0/0"
+    network_interface_id = module.nat.nat_network_interface_id
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  count             = var.test_count
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.10.${count.index + 2}.0/24"
+  tags              = { Name = "test-private-subnet-${count.index}" }
+  availability_zone = data.aws_availability_zones.azs.names[count.index % length(data.aws_availability_zones.azs.names)]
+}
+
+resource "aws_route_table_association" "private_route_table_association" {
+  count          = var.test_count
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private_route_table[count.index].id
+}
